51a071929fbe8fd3a8fca1b84e82829ff902047b
William Klimpert <william_klimpert@brown.edu>
2019-01-15 16:22:58 -0500

    Up to 1.0
    Finished adding people,
    Started adding JQuery

---
+++ main.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+
+const filterWeeks = () => {
+  //Filter week one
+  if ($(".dropdown-content span").hasClass("filtered")) {
+    $("#week-one").hide();
+  } else {
+    $("#week-one").show();
+  }
+  
+  noMoreGirls();
+  //Filter other weeks
+  filterOtherWeeks();
+}
+
+const filterOtherWeeks = () => {
+  if ($("#week-two .colton-looks .girls").children(":visible").length == 0) {
+    $("#week-two .colton-looks .colton-looks-img").hide();
+    console.log($("#week-two .colton-looks .girls").children(":visible").length);
+  } else {
+    $("#week-two .colton-looks .colton-looks-img").show();
+  }
+};
+
+const noMoreGirls = () => {
+  if ($("#remaining-girls .girls").children(":visible").length == 0) {
+    $("#title").addClass("chris-background");
+  } else {
+    $("#title").removeClass("chris-background");
+  }
+}
+
+$("#title").on("click", () => {
+  $(".william").toggle();
+
+})
+
+$(".dropdown").on("mouseover", () => {
+  $(".dropdown-content").show();
+  $(".dropbtn").addClass("hover");
+})
+
+$(".dropdown").on("mouseleave", () => {
+  $(".dropdown-content").hide();
+  $(".dropbtn").removeClass("hover");
+})
+
+$(".dropdown-content span").on("click", event => {
+  $(event.currentTarget).toggleClass("filtered");
+})
+
+$("#filterMat").on("click", () => {
+  $(".mat").toggle();
+  filterWeeks();
+})
+
+$("#filterWilliam").on("click", () => {
+  $(".william").toggle();
+  filterWeeks();
+})
+
+$("#filterNell").on("click", () => {
+  $(".nell").toggle();
+  filterWeeks();
+})
+
+$("#filterErika").on("click", () => {
+  $(".erika").toggle();
+  filterWeeks();
+})
+
+$("#filterKennedy").on("click", () => {
+  $(".kennedy").toggle();
+  filterWeeks();
+})
+
+$("#filterRejected").on("click", () => {
+  $(".rejected").toggle();
+  filterWeeks();
+})
+
+});
